b3d4e6a18aa88bc7337c1a1ad6d2b41bf001061e
Ziga Avsec <avsec@in.tum.de>
2019-10-20 15:22:10 -0700

    implemented stacked instances for chexmix,meme,homer

--- basepair/exp/chipnexus/comparison.py
+++ basepair/exp/chipnexus/comparison.py
@@ -7,8 +7,11 @@ def read_chexmix_dfi(expriment_events_path):
     subtype_cols = dfi.SubtypePoint.str.split(":", expand=True)
 
     dfi['chrom'] = subtype_cols[0]
-    dfi['pattern_center_abs'] = subtype_cols[1].astype(int)
+    
     dfi['strand'] = subtype_cols[2]
+    dfi['pattern_center_abs'] = subtype_cols[1].astype(int)
+    # fix the strand offset 
+    dfi['pattern_center_abs'] = dfi['pattern_center_abs'] - (dfi['strand'] == '-')
     dfi['profile_score'] = dfi['experiment_log2Fold']
     dfi['motif_score'] = dfi['SubtypeMotifScore']
     dfi['pattern_name'] = (dfi['SubtypeName'].str.replace("Subtype", "").astype(int) + 1).astype(str)
@@ -32,7 +35,9 @@ def read_fimo_dfi(file_path):
     df = pd.read_csv(file_path, sep='\t', comment='#')
     df['chrom'] = df['sequence_name']
     df['end'] = df['stop']
-    df['pattern_center_abs'] = interval_center(df).astype(int)
+    
+    # fix the strand offset (strange)
+    df['pattern_center_abs'] = interval_center(df).astype(int) - (df['strand'] == '-')
     df['pattern_name'] = df['motif_alt_id'].str.replace("MEME-", "")
     df = df[df.chrom.isin(all_chr)]
     return df
@@ -70,3 +75,54 @@ def read_homer(file_path):
     with open("motif1.motif.txt") as f:
         l = f.readline()
     return PWM(df + 0.01, name="P={}".format(l.split(",P:")[1].strip()))
+
+
+# peak size
+def read_homer_dfi(fpath, peak_fpath, tf=None, exclude_chr=['chrX', 'chrY'], peak_size=1000):
+    from tqdm import tqdm
+    import pandas as pd
+
+    df = pd.read_csv(fpath, sep='\t')
+    # read the peaks file
+    peaks = pd.read_csv(peak_fpath, sep='\t', header=None, names=['chrom', 'start', 'end'])
+    
+    # TODO - resize start and end
+    df = pd.concat([df, peaks.iloc[df.PositionID - 1].reset_index()], axis=1)
+
+    df = df.rename(columns={"MotifScore": 'score', "Strand": 'strand'})
+    
+    df['pattern_center_abs'] = (df['start'] + df['end']) // 2 + df['Offset']
+    
+    df['pattern_len'] = df['Sequence'].str.len()
+    
+    rc_shift = ( df['pattern_len'] - 1) * (df['strand'] == '-')
+    df['pattern_start_abs'] = df['pattern_center_abs']  - rc_shift
+    df['pattern_end_abs'] = df['pattern_start_abs'] + df['pattern_len']
+    
+    # re-compute the center position
+    add_offset = df.strand.map({"+": 1, "-": 0})
+    delta = (df.pattern_end_abs + df.pattern_start_abs) % 2
+    df['pattern_center_abs'] = (df.pattern_end_abs + df.pattern_start_abs) // 2 + add_offset * delta
+    
+    # relative pattern center
+    # update start
+    center = (df['start'] + df['end']) // 2
+    df['start'] = center - peak_size // 2
+    df['end'] = center + peak_size // 2 + (peak_size % 2)
+ 
+    df['pattern_center'] = df['pattern_center_abs'] - df['start']
+    
+    df['example_idx'] = df.PositionID - 1
+    
+    if tf is not None:
+        df['pattern_name'] = tf + '/' + df['Motif Name'].str.split("-", expand=True)[0] 
+        
+        
+    # exclude chromosomes
+    keep_idx = pd.Series(np.arange(len(peaks)))[~peaks.chrom.isin(exclude_chr)]
+    keep_idx = pd.Series(data=np.arange(len(keep_idx)), index=keep_idx.index)
+    example_idx_map = dict(keep_idx)
+    
+    df = df[df.example_idx.isin(example_idx_map)]
+    df['example_idx'] = df['example_idx'].map(example_idx_map)
+    return df
