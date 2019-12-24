c7712aad8db31cace94e915febc8c3dfd6e8df68
Ziga Avsec <avsec@in.tum.de>
2019-12-24 01:06:02 +0000

    add new periodicity functions, extend 11.2-method-comparison-pwm.ipynb, perform mappability analysis

--- basepair/modisco/periodicity.py
+++ basepair/modisco/periodicity.py
@@ -6,6 +6,51 @@ import matplotlib.pyplot as plt
 from basepair.plot.profiles import extract_signal
 from basepair.plot.tracks import plot_tracks
 from basepair.plot.heatmaps import heatmap_importance_profile, normalize
+from typing import Tuple
+
+
+def periodicity_ft(signal: np.array
+                  ) -> Tuple[np.array, np.array]:
+    """Compute the periodicity of a genomic signal.
+    
+    See the following tutorial:
+    https://colab.research.google.com/drive/1s158_1RxkKkIIlKrxdCJ3BebH7CvK3pK#scrollTo=kVcWfnr0xSxi
+
+    Args:
+      signal: signal at base-resolution.
+
+    Returns 
+      A tuple of periodicity (t0) in nucleotide unit and 
+        the power spectrum.
+    """
+    power_spectrum = np.abs(np.fft.rfft(signal))**2
+    freq = np.fft.rfftfreq(len(signal))
+    # Return everything except the 0-frequency
+    # which is equal to:
+    # np.abs(np.fft.rfft(signal)[0]) == np.sum(signal)
+    
+    # Last element in this list is the one with 
+    # largest frequency and hence smallest periodicity (t0).
+    return 1 / freq[1:], power_spectrum[1:]
+
+
+def periodicity_ft_summit(t0: np.array,
+                          power_spectrum: np.array
+                         ) -> Tuple[float, float]:
+    """Get the periodicity summit together with the 
+    expected precision periodicity +- error.
+    
+    Args:
+      t0, power_spectum: Periodicity and power spectrum 
+        returned by periodicity_ft.
+      
+    Returns:
+      Tuple containing periodicity and the +- 10bp error.
+    """
+    i_max = np.argmax(power_spectrum)
+    error = max(t0[i_max] - t0[i_max+1],
+                t0[i_max - 1] - t0[i_max]) / 2
+    return t0[i_max], error
 
 
 def smooth(y, box_pts):
@@ -31,20 +76,14 @@ def compute_power_spectrum(pattern, task, data):
     smooth_part = smooth(agg_profile, 10)
     oscilatory_part = agg_profile - smooth_part
 
-    freq = np.fft.fftfreq(agg_profile[102:].shape[-1])
-
-    freq = freq[:49]
-    t0 = 1 / freq
-    ps = np.abs(np.fft.fft(oscilatory_part[102:])[:49])**2 + np.abs(np.fft.fft(oscilatory_part[:98])[:49])**2
-    return ps, t0, freq
-    # plt.savefig('nanog-fft.png', dpi=300)
-    # plt.savefig('nanog-fft.pdf')
+    t0, ps = periodicity_ft(oscilatory_part)
+    return ps, t0, 1 / t0
 
 
 def periodicity_10bp_frac(pattern, task, data):
     ps, t0, freq = compute_power_spectrum(pattern, task, data)
-    assert t0[9] == 10.88888888888889
-    return ps[9] / ps.sum()
+    assert t0[18] == 10.526315789473685
+    return ps[18] / ps.sum()
 
 
 def plot_power_spectrum(pattern, task, data):
@@ -63,7 +102,6 @@ def plot_power_spectrum(pattern, task, data):
 
     agg_profile = agg_profile - agg_profile.mean()
     agg_profile = agg_profile / agg_profile.std()
-    freq = np.fft.fftfreq(agg_profile[102:].shape[-1])
 
     smooth_part = smooth(agg_profile, 10)
     oscilatory_part = agg_profile - smooth_part
@@ -83,11 +121,12 @@ def plot_power_spectrum(pattern, task, data):
     # plt.savefig('nanog-agg-profile.pdf')
 
     fft_fig = plt.figure(figsize=(11, 2))
-    plt.plot(1 / freq[:49], np.abs(np.fft.fft(oscilatory_part[102:])[:49])**2 + np.abs(np.fft.fft(oscilatory_part[:98])[:49])**2, "-o")
+    t0, power_spectrum = periodicity_ft(oscilatory_part)
+    plt.plot(t0, power_spectrum, "-o")
     plt.xlim([0, 50])
     plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(25, integer=True))
     plt.grid(alpha=0.3)
-    plt.xlabel("1/Frequency [bp]")
+    plt.xlabel("1/frequency (bp)")
     plt.ylabel("Power spectrum")
     plt.title("Power spectrum")
     plt.gcf().subplots_adjust(bottom=0.4)
